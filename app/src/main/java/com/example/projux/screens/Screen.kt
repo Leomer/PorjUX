f99e8c934795a9a5dba1f71e68d15566ce00377a
Leomer <leomer331ff@hotmail.com>
2023-12-05 21:57:27 -0500

    feat: add finish purchase flow

--- app/src/main/java/com/example/projux/screens/Screen.kt
+++ app/src/main/java/com/example/projux/screens/Screen.kt
@@ -2,8 +2,14 @@ package com.example.projux.screens
 
 sealed class Screen (val route: String) {
     object HomeScreen: Screen("home_screen")
+    //Compra
     object PurchaseScreen: Screen("purchase_screen")
+    object PurchaseReadScreen: Screen("purchase_read_screen")
+    object PurchaseOKScreen: Screen("purchase_ok_screen")
+    //Compra con Qr
     object QrScreen: Screen("qr_screen")
+    object QrScreenImage: Screen("qr_image_screen")
+
     object CancelScreen: Screen("cancel_screen")
     object PreAuthorizationScreen: Screen("pre_authorization_screen")
     object ReportScreen: Screen("report_screen")
